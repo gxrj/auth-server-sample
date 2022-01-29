c86de00dbc0ab9dda60ab450bba22555d3de0eba
gxrj <gsouzafone@gmail.com>
2022-01-29 03:55:42 -0300

    feat: Enable access to users loginPage and agents LoginPage

--- src/main/java/com/example/authserversample/config/AppSecurityConfig.java
+++ src/main/java/com/example/authserversample/config/AppSecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,45 +15,38 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity( debug = true )
 @Configuration
 public class AppSecurityConfig {
 
     @Bean
-    @Order( 1 )
-    SecurityFilterChain userSecurityFilterChain( final HttpSecurity http )
+    SecurityFilterChain userSecurityFilterChain( HttpSecurity http )
     throws Exception 
     {
         http
         .authorizeHttpRequests(
-            authRequests -> authRequests.mvcMatchers( "/login" ).permitAll()
+            authRequests -> authRequests
+                                .mvcMatchers( "/login", "/agent/login", "/css/**", "/img/**" )
+                                .permitAll()
                                 .anyRequest().authenticated()
         )
         .cors().disable()
+        .csrf().disable()
         .sessionManagement()
                 .sessionCreationPolicy( SessionCreationPolicy.NEVER )
         .and()
-        .formLogin().loginPage( "/login" );
-
-        return http.build();
-    }
-
-    @Bean
-    @Order( 2 )
-    SecurityFilterChain agentSecurityFilterChain( final HttpSecurity http )
-            throws Exception
-    {
-        http
-                .authorizeHttpRequests(
-                        authRequests -> authRequests.mvcMatchers( "/agent/login" ).permitAll()
-                                .anyRequest().authenticated()
+        .exceptionHandling()
+                .defaultAuthenticationEntryPointFor( /* Login para usuários */
+                        new LoginUrlAuthenticationEntryPoint( "/login" ),
+                        new AntPathRequestMatcher("/login" )
                 )
-                .cors().disable()
-                .sessionManagement()
-                .sessionCreationPolicy( SessionCreationPolicy.NEVER )
-                .and()
-                .formLogin().loginPage( "/login-agent" );
+                .defaultAuthenticationEntryPointFor( /* Login para funionários públicos */
+                        new LoginUrlAuthenticationEntryPoint( "/agent/login" ),
+                        new AntPathRequestMatcher("/agent/login" )
+                );
 
         return http.build();
     }
@@ -69,7 +61,14 @@ public class AppSecurityConfig {
                                 .roles( "USER" )
                                 .build();
 
-        return new InMemoryUserDetailsManager( user );
+        UserDetails agent = User.builder()
+                .authorities( List.of() )
+                .username( "agent" )
+                .password( encoder().encode( "123" ) )
+                .roles( "AGENT" )
+                .build();
+
+        return new InMemoryUserDetailsManager( user, agent );
     }
 
     @Bean
