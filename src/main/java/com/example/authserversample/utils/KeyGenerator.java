102554bdb4ebe76ff9c9e3bdebf2113803f7c48d
Gustavo de Souza Lemos <32879380+gxrj@users.noreply.github.com>
2022-02-01 00:40:37 -0300

    Update KeyGenerator.java
    Exponsing extra info, auth-server still not able to find jws key properly

--- src/main/java/com/example/authserversample/utils/KeyGenerator.java
+++ src/main/java/com/example/authserversample/utils/KeyGenerator.java
@@ -2,9 +2,13 @@ package com.example.authserversample.utils;
 
 import java.util.UUID;
 
+import com.nimbusds.jose.Algorithm;
+import com.nimbusds.jose.Requirement;
 import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 
 public class KeyGenerator {
     
@@ -13,6 +17,8 @@ public class KeyGenerator {
         try{
             return new ECKeyGenerator( Curve.P_256 )
                             .keyID( UUID.randomUUID().toString() )
+                            .keyUse( KeyUse.SIGNATURE )
+                            .algorithm( new Algorithm( SignatureAlgorithm.ES256.toString() , Requirement.RECOMMENDED ) )
                             .generate();
         }
         catch( Exception ex ){
